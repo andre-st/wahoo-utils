31f94a8d895ec7a95c2ab9fb5c53d1ff9f8e39d5
andre-st <datakadabra@gmail.com>
2025-08-30 13:06:55 +0200

    Check some requirements

--- setup.sh
+++ setup.sh
@@ -1,34 +1,39 @@
 #/bin/bash
 
-# Change to project directory
+
+# Stop script on errors:
+set -e
+
+
+# Check environment:
+if ! command -v pip >/dev/null 2>&1; then
+	echo "[ERROR] Missing 'pip' on your system. For Ubuntu/Debian run: sudo apt install python3-pip" >&2
+	exit 1
+fi
+
+
+# Change to project directory:
 pushd "$(dirname "$(realpath "$0")")" > /dev/null || exit 1
 
 
-# Base
-
+# Base:
 mkdir -p routes
+chmod u+x *.sh *.py
 
 
 # Install dependencies in local project directory:
-
 python -m venv "local"
-
 source "local/bin/activate"
-
 # Indirect deps:
 #    shapely <- geopandas
 #    pandas  <- geopandas
-
 pip install geopandas gpxpy lxml osmnx geopy pygeohash adbutils
-
-chmod +x *.py *.sh
 
 
 # Android Debug Bridge:
-
 if [ -d ./local/opt/platform-tools ]
 then
-	echo "ADB already installed"
+	echo "[INFO] ADB already installed"
 else
 	zipname=platform-tools-latest-linux.zip
 	wget "https://dl.google.com/android/repository/$zipname"
